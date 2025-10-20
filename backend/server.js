6dc6c384a4adedbe1a8636788e826a99cb1b4fbe
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2025-10-20 11:24:16 +0800

    Connected MONGODB database

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDb = require("./config/dbConnection");
 
 const app = express();
-
+dotenv.config();
 const router = express.Router();
 
 const PORT = 5000;
 
 app.use(express.json());
+
+connectDb();
 app.listen(PORT, () => {
     console.log(`Port is running on port ${PORT}`)
 })
