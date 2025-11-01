8b186c4bc71fd1332d2fb07c7f5b8f2b131f52ba
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2025-11-01 22:22:17 +0800

    ADDED [CART ROUTE] product quantity logic

--- backend/controllers/cartControllers.js
+++ backend/controllers/cartControllers.js
@@ -8,6 +8,12 @@
 const User = require("../models/userModel");
 const Product = require("../models/productModel");
 
+// [ CART CONTROLLERS ]
+//
+// [1] GET USER CART
+// @desc Fetches user cart
+// @route GET /api/cart/:userId/
+// @access PRIVATE
 const getCart = async (req, res) => {
     const {userId} = req.params;
 
@@ -25,6 +31,10 @@ const getCart = async (req, res) => {
         }
 }
 
+// [2] ADD TO CART
+// @desc Adds a product to user's cart
+// @route POST /api/cart/:userId/add
+// @access PRIVATE
 const addToCart = async (req, res) => {
     const { userId } = req.params;
   const { productId, quantity = 1 } = req.body;
@@ -64,4 +74,58 @@ const addToCart = async (req, res) => {
   }
 }
 
-module.exports = {addToCart, getCart};
+// [3] UPDATE PRODUCT QUANTITY
+// @desc Updates quantity of product in user's cart
+// @route PUT /api/cart/:userId/update/:productID
+// @access PRIVATE
+const updateProductQuantity = async (req, res) => {
+    const {quantity} = req.body;
+    const {userId, productId} = req.params;
+
+    try{
+      // Validate req parameters
+      if(!userId || !productId || quantity === undefined) return res.status(400).json({message: "All fields are required"});
+
+      // Validate that stock is a positive number
+      if (typeof quantity !== "number" || quantity < 0) {
+        return res.status(400).json({ message: "Invalid quantity value" });
+      }
+      const user = await User.findById(userId);
+      
+      // If user does not exist
+      if (!user) return res.status(400).json({message: "User does not exist"});
+
+      const product = await Product.findById(productId);
+
+      // If product does not exist
+      if (!product) return res.status(400).json({message: "Product does not exist"});
+
+      // Copy current user's cart
+      const updatedCart = [...user.cart];
+      
+      // Iterate through the User model cart field 
+      const productIndex = user.cart.findIndex(
+        (item) => item.productId.toString() === productId
+      );
+
+      if (productIndex === -1)
+      return res.status(404).json({ message: "Product not found in cart" });
+
+      // Update quantity (make sure it's positive)
+      updatedCart[productIndex].quantity = Math.max(1, Number(quantity));
+      
+      // Update user cart and save
+      user.cart = updatedCart;
+      await user.save();
+
+
+      res.status(200).json({message: "Cart updated successfully", 
+      cart: user.cart
+      })
+     
+    }catch(err){
+      res.status(500).json({message: err.message});
+    }
+}
+
+module.exports = {addToCart, getCart, updateProductQuantity};
--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
-const { getCart, addToCart } = require("../controllers/cartControllers");
+const { getCart, addToCart, updateProductQuantity } = require("../controllers/cartControllers");
 
 const router = express.Router();
 
 // [ROUTE ENDPOINTS]
 router.get("/:userId/", getCart);
 router.post("/:userId/add", addToCart);
-
+router.patch("/:userId/update/:productId", updateProductQuantity)
 module.exports = router;
