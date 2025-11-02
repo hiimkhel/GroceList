404686b3a4a7c1b6657a8dbb84d6b7b23da8d528
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2025-11-02 12:53:50 +0800

    [FEATURE] added a errorHandler middleware

---
+++ backend/middleware/errorHandler.js
@@ -0,0 +1,18 @@
+/**
+ * @file errorHandler.js
+ * @description Global error handling middleware
+ */
+
+const errorHandler = (err, req, res, next) => {
+     console.error(`[ERROR] ${err.message}`);
+
+     const statusCode = req.statusCode !== 200 ? res.statusCode : 500;
+
+    res.status(statusCode).json({
+        success: false,
+        message: err.message || "Server Error",
+        stack: process.env.NODE_ENV === "production" ? null : err.stack;
+    })
+}
+
+module.exports = errorHandler;
