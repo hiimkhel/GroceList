d913c1d8b4bb8f3408f58d298ea8cfd4ce43f19b
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2025-11-07 15:40:03 +0800

    ADDED sidebar component structure

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom';
+import Logo from '../assets/GrocelistLogo.svg';
+import React from 'react';
+
+const SideBar: React.FC = () => {
+
+    return(
+    <div>
+        <div>
+            <Link to="/">
+                <img src="../assets/Hamburger.svg"></img> {/* Sidebar menu */}
+            </Link>
+            
+            <div className='h-[200px] w-[400px] rounded-lg bg-white'></div> {/* User profile card */}
+            <div> {/* Menu */}
+                <Link to="/marketplace">
+                    <img src="../assets/Marketplace.svg"></img>
+                    <h3>Marketplace</h3>
+                </Link>
+                <Link to="/checklist">
+                    <img src="../assets/Checklist.svg"></img>
+                    <h3>Checklist</h3>
+                </Link>
+                <Link to="/cart">
+                    <img src="../assets/ShoppingCart.svg"></img>
+                    <h3>Shopping Cart</h3>
+                </Link>
+            </div> 
+        </div>
+        {/* Lower Section */}
+        <div>   
+            <div className="w-4/5 mx-auto bg-cyan-500">
+                <img></img>
+                <h1>Logout</h1>
+            </div>
+            
+        </div>
+    </div>
+)
+}
