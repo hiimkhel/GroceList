8e2b631814235047a19941f79008a4e75551a018
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2025-11-08 21:00:45 +0800

    COMMENTED final routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,11 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/auth/login" element={<LoginPage>} /> */}
+        {/* <Route path="/auth/register" element={<RegisterPage/>} /> */}
         {/* <Route path="/marketplace" element={<Marketplace />} /> */}
+        {/* <Route path="/lists" element={<GroceryListPage />} /> */}
+        {/* <Route path="/user" element={<CartPage />} /> */}
       </Routes>
     </BrowserRouter>
   );
