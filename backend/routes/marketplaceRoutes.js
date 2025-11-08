446288c6d3c7c75451d36d9e8fcdad754b1f9338
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2025-11-08 14:08:06 +0800

    ADDED get a product

--- backend/routes/marketplaceRoutes.js
+++ backend/routes/marketplaceRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const {getAllProducts} = require("../controllers/marketplaceControllers")
+const {getAllProducts, getProduct} = require("../controllers/marketplaceControllers")
 const router = express.Router();
 
 // [ROUTE ENDPOINTS]
 router.get("/", getAllProducts);
-
+router.get("/:productId", getProduct);
 module.exports = router;
